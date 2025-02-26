5596427013dd10bd16176fb9233f18a0fd593479
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-02-26 19:05:44 -0300

    fix bad cart icon position on mobile view

--- src/components/Layout/NavBar/index.jsx
+++ src/components/Layout/NavBar/index.jsx
@@ -7,7 +7,7 @@ export const NavBar = () => {
     <nav className={`container-xl d-flex row row-gap-3 align-items-start align-items-md-center py-1 `}>
       <HeaderLinks responsiveClass="col-8 col-md-3 order-1"></HeaderLinks>
       <SearchInput responsiveClass="col-12 col-md-6 order-3 order-md-2 pb-2 pb-md-0"></SearchInput>
-      <UserControls responsiveClass="col-4 col-md-3 order-2 order-md-3 pt-4 pt-md-0 justify-content-end pe-md-3"></UserControls>
+      <UserControls responsiveClass="col-4 col-md-3 order-2 order-md-3 pt-3 pt-md-0 justify-content-end pe-md-"></UserControls>
     </nav>
   );
 };
